d7d6f53a6062aa7058a930f3a09e5fe9ec3208d3
Juan <juan.pasgut.workspace@gmail.com>
2025-12-18 10:16:00 +0100

    a

---
+++ servidor/php/app/laravel/instituto/resources/views/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>adouiashiduhadh </h1>
+  <p>{{ $num }}</p>
+  <p>{{ $nombre }}</p>
+</body>
+</html>
---
+++ servidor/php/app/laravel/instituto/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/saludo', function () {
+    return "hola";
+});
+
+Route::view("/ver", "saludo");
+
+Route::get("/main", [MainController::class, "index"]);
+
+Route::fallback(function () {
+  $ruta = request()->url();
+  return "te has perdido gang, $ruta no existe";
+});
